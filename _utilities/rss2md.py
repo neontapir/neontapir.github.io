ed8e5567181456cd71ca9f471509369a4f76d2a2
Chuck Durfee <neontapir@gmail.com>
2020-04-14 13:41:13 -0600

    add Asciidoc, improve posting script

--- _utilities/rss2md.py
+++ _utilities/rss2md.py
@@ -1,28 +1,11 @@
 #!/usr/bin/env python3
 import feedparser
 import html2markdown
+import os.path
+from os import path
 import re
 
-# <item>
-#   <itunes:title>The Accountability Dial</itunes:title>
-#   <title>The Accountability Dial</title>
-#   <description><![CDATA[<p>In this episode, Chuck illustrates the use of Jonathan Raymond's Accountability Dial technique, with Manager Tools feedback examples as well.</p><p><a rel="payment" href="https://www.patreon.com/agilechuckwagon">Support the show</a> (https://www.patreon.com/agilechuckwagon)</p>]]></description>
-#   <content:encoded><![CDATA[<p>In this episode, Chuck illustrates the use of Jonathan Raymond's Accountability Dial technique, with Manager Tools feedback examples as well.</p><p><a rel="payment" href="https://www.patreon.com/agilechuckwagon">Support the show</a> (https://www.patreon.com/agilechuckwagon)</p>]]></content:encoded>
-#   <itunes:image href="https://storage.buzzsprout.com/variants/olq4a2ik13r4w1yqfllusnwim2ae/8d66eb17bb7d02ca4856ab443a78f2148cafbb129f58a3c81282007c6fe24ff2?.jpg" />
-#   <itunes:author>Chuck Durfee</itunes:author>
-#   <itunes:summary>In this episode, Chuck illustrates the use of Jonathan Raymond&#39;s Accountability Dial technique, with Manager Tools feedback examples as well.</itunes:summary>
-#   <enclosure url="https://www.buzzsprout.com/277882/3008284-the-accountability-dial.mp3?blob_id=11193598" length="12358978" type="audio/mpeg" />
-#   <guid isPermaLink="false">Buzzsprout-3008284</guid>
-#   <pubDate>Wed, 25 Mar 2020 09:00:00 -0600</pubDate>
-#   <itunes:duration>1027</itunes:duration>
-#   <itunes:keywords>management, discipline, feedback, categories, scale</itunes:keywords>
-#   <itunes:season>10</itunes:season>
-#   <itunes:episode>6</itunes:episode>
-#   <itunes:episodeType>full</itunes:episodeType>
-#   <itunes:explicit>false</itunes:explicit>
-# </item>
-
-def parse_value(item):
+def build_post_body(item):
   value = item["content"][0]['value']
   value = re.sub('<p>This episode is sponsored by .*</p>', '', value)
   value = re.sub('<a rel="payment" href="https://www.patreon.com/agilechuckwagon">.+</a>', '', value)
@@ -44,8 +27,21 @@ def parse_season_episode(item, title):
     episode = item["itunes_episode"]
   return (season, episode)
 
+draftsFolderName = '../_drafts/'
+
+def ensure_drafts_folder():
+  if not path.exists(draftsFolderName):
+    os.makedirs(draftsFolderName)
+
+def build_fileName(time, title):
+  fileName = str(time.tm_year) + '-' + ("%02d" % time.tm_mon) + '-' + ("%02d" % time.tm_mday) + '-' + title + '.md'
+  fileName = re.sub("[/']", '', fileName)
+  fileName = fileName.replace(' ', '-').replace('---', '-')
+  return fileName
+
 # rss_url = "277882.rss.txt" 
 rss_url = "https://feeds.buzzsprout.com/277882.rss"
+ensure_drafts_folder()
 
 feed = feedparser.parse( rss_url )
 items = feed["items"]
@@ -61,20 +57,18 @@ for item in items:
 
   title = item[ "title" ].replace(':', " -") #.encode('utf-8')
   
-  fileName = str(time.tm_year) + '-' + ("%02d" % time.tm_mon) + '-' + ("%02d" % time.tm_mday) + '-' + title + '.md'
-  fileName = re.sub("[/']", '', fileName)
-  fileName = fileName.replace(' ', '-').replace('---', '-')
-  fileName = '../_posts/' + str(time.tm_year) + '/' + fileName
+  fileName = build_fileName(time, title)
+  postFolderName = '../_posts/' + str(time.tm_year) + '/'
+  if path.exists(postFolderName + fileName):
+    continue
 
   duration = str(round(float(item["itunes_duration"]) / 60))
 
-  value = parse_value(item)
+  post_body = build_post_body(item)
 
-  episode_info = parse_season_episode(item, title)
-  season = episode_info[0]
-  episode = episode_info[1]
-  
-  f = open(fileName,'w') 
+  (season, episode) = parse_season_episode(item, title)
+
+  f = open(draftsFolderName + fileName,'w') 
   f.write('---\ntype: post\nlayout: post\ntitle: ' + title + '\n')
   if item.has_key('image'):
     eye_catch = item['image']['href']
@@ -97,6 +91,27 @@ _edit_last: "1"
 
 ''')
   f.write('### Agile Chuck Wagon, season ' + season + ', episode ' + episode + '\n\n')
-  f.write(value + '\n')
+  f.write(post_body + '\n')
 
 print('...done')
+
+# RSS item sample:
+#
+# <item>
+#   <itunes:title>The Accountability Dial</itunes:title>
+#   <title>The Accountability Dial</title>
+#   <description><![CDATA[<p>In this episode, Chuck illustrates the use of Jonathan Raymond's Accountability Dial technique, with Manager Tools feedback examples as well.</p><p><a rel="payment" href="https://www.patreon.com/agilechuckwagon">Support the show</a> (https://www.patreon.com/agilechuckwagon)</p>]]></description>
+#   <content:encoded><![CDATA[<p>In this episode, Chuck illustrates the use of Jonathan Raymond's Accountability Dial technique, with Manager Tools feedback examples as well.</p><p><a rel="payment" href="https://www.patreon.com/agilechuckwagon">Support the show</a> (https://www.patreon.com/agilechuckwagon)</p>]]></content:encoded>
+#   <itunes:image href="https://storage.buzzsprout.com/variants/olq4a2ik13r4w1yqfllusnwim2ae/8d66eb17bb7d02ca4856ab443a78f2148cafbb129f58a3c81282007c6fe24ff2?.jpg" />
+#   <itunes:author>Chuck Durfee</itunes:author>
+#   <itunes:summary>In this episode, Chuck illustrates the use of Jonathan Raymond&#39;s Accountability Dial technique, with Manager Tools feedback examples as well.</itunes:summary>
+#   <enclosure url="https://www.buzzsprout.com/277882/3008284-the-accountability-dial.mp3?blob_id=11193598" length="12358978" type="audio/mpeg" />
+#   <guid isPermaLink="false">Buzzsprout-3008284</guid>
+#   <pubDate>Wed, 25 Mar 2020 09:00:00 -0600</pubDate>
+#   <itunes:duration>1027</itunes:duration>
+#   <itunes:keywords>management, discipline, feedback, categories, scale</itunes:keywords>
+#   <itunes:season>10</itunes:season>
+#   <itunes:episode>6</itunes:episode>
+#   <itunes:episodeType>full</itunes:episodeType>
+#   <itunes:explicit>false</itunes:explicit>
+# </item>
